411ac8656f219645ee30af0bb60dd871e797f70b
liuwin7 <liuwin7@163.com>
2018-08-28 17:51:56 +0800

    修改配置文件

--- register_tasks.js
+++ register_tasks.js
@@ -9,7 +9,7 @@ module.exports = shipit => {
         shipit.config = shipit.config || {}; // 读取配置
         var cmd = 'cd /home/fdliu/server/gruntDemo/current;' +
             'npm install --production;' +
-            'pm2 restart ./bin/www --';
+            'pm2 restart pm2.json';
         return shipit.remote(cmd);
     }
 };
